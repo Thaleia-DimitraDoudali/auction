f901a0813cdbc52d960f16daf1e7d729a67c2ad2
Christos-Michas <cmichas3@gmail.com>
2015-04-07 14:55:04 +0300

    Changed something, don't remember what :P

--- src/server/MessageServerHandler.java
+++ src/server/MessageServerHandler.java
@@ -77,18 +77,6 @@ public class MessageServerHandler {
 			myString.split("\\s+");
 			//remove bidder from interested and regtable
 			break;
-		case '4':
-			break;
-		case '5':
-			break;
-		case '6':
-			break;
-		case '7':
-			break;
-		case '8':
-			break;
-		case '9':
-			break;
 		default:
 			break;
 		}
